a982762a4b902fc86be2b7933882a20a5c4b9c88
jaysonzhin09 <135104406+jaysonzhin09@users.noreply.github.com>
2023-06-13 15:19:54 +0800

    Create JsAssessment.js

---
+++ JsAssessment.js
@@ -0,0 +1,50 @@
+/*
+Assessment Requirements
+1. Create a variable that can hold a number of NFT's. What type of variable might this be?
+2. Create an object inside your mintNFT function that will hold the metadata for your NFTs. 
+   The metadata values will be passed to the function as parameters. When the NFT is ready, 
+   you will store it in the variable you created in step 1
+3. Your listNFTs() function will print all of your NFTs metadata to the console (i.e. console.log("Name: " + someNFT.name))
+4. For good measure, getTotalSupply() should return the number of NFT's you have created
+*/
+
+// create a variable to hold your NFT's
+const NFTs = []
+// this function will take in some values as parameters, create an
+// NFT object using the parameters passed to it for its metadata, 
+// and store it in the variable above.
+function mintNFT (name,primary,secondary,melee) {
+ const NFT = {
+    "name":name,
+    "primary":primary,
+    "secondary":secondary,
+    "melee":melee
+ } 
+ NFTs.push(NFT);
+ console.log("Minted: " +name);
+}
+
+// create a "loop" that will go through an "array" of NFT's
+// and print their metadata with console.log()
+function listNFTs () {
+    for(let i = 0; i < NFTs.length; i++){
+        console.log("\nID: "+(i+1)); 
+        console.log("Name: "+NFTs[i].name);
+        console.log("Primary Weapon: "+NFTs[i].primary);
+        console.log("Secondary Weapon: "+NFTs[i].secondary);
+        console.log("Melee Weapon: "+NFTs[i].melee);
+    }
+}
+
+// print the total number of NFTs we have minted to the console
+function getTotalSupply() {
+console.log("\n NFTs Currently Minted: " + NFTs.length);
+}
+
+// call your functions below this line
+mintNFT("Nidus Prime","Bubonico","Catabolyst","Praedos");
+mintNFT("Trinity Prime","Nataruk","Epitaph","Azothane");
+mintNFT("Saryn Prime","Tenet Arca Plasmor","Sporelacer","Glaive Prime");
+mintNFT("Volt Prime","Zenith","Laetum","Redeemer Prime");
+listNFTs();
+getTotalSupply();
